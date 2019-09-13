86f78e06eabd14a796952ca6cb8cc56fd036e7bd
kawshar <kawshar@joomshaper.com>
2019-09-13 17:00:48 +0600

    remove unnecessary css

--- administrator/components/com_media/layouts/toolbar/create-folder.php
+++ administrator/components/com_media/layouts/toolbar/create-folder.php
@@ -16,6 +16,6 @@ HTMLHelper::_('webcomponent', 'system/joomla-toolbar-button.min.js', ['version' 
 
 $title = Text::_('COM_MEDIA_CREATE_NEW_FOLDER');
 ?>
-<joomla-toolbar-button class="btn btn-sm btn-info" onclick="MediaManager.Event.fire('onClickCreateFolder');">
+<joomla-toolbar-button class="btn btn-info" onclick="MediaManager.Event.fire('onClickCreateFolder');">
 	<span class="icon-folder-close"></span> <?php echo $title; ?>
 </joomla-toolbar-button>
