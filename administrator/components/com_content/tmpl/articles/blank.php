11318de36deeec03e718dd7b164c3398be9b717b
Delowar Hosain <delowar6494@gmail.com>
2019-09-17 20:53:19 +0600

    card quick overview box design updated

--- administrator/components/com_content/tmpl/articles/blank.php
+++ administrator/components/com_content/tmpl/articles/blank.php
@@ -488,49 +488,78 @@ HTMLHelper::_('webcomponent', 'system/joomla-alert.es6.min.js', array('version'=
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="col-4">
-            <div class="j-card j-card-has-hover">
-                <div class="j-card-media">
-                    <img src="https://picsum.photos/id/684/400/300" alt="">
-                </div>
-                <div class="j-card-body j-card-body-has-padding">
-                    <div class="j-card-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem earum ex expedita incidunt minus modi odit pariatur provident quasi, vero!
-                    </div>
-                </div>
-                <span class="j-card-divider"></span>
+    <hr>
 
-                <div class="j-card-item-group">  <!-- Possible ext class:  j-no-margin, j-no-padding -->
-                    <div class="j-card-item">
-                        <span>Hello</span>
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <div class="j-card j-card-has-hover">
+                    <div class="j-card-media">
+                        <img src="https://picsum.photos/id/684/400/300" alt="">
                     </div>
-                    <div class="j-card-item">
-                        <i class="fab fa-facebook"></i>
+                    <div class="j-card-body j-card-body-has-padding">
+                        <div class="j-card-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem earum ex expedita incidunt minus modi odit pariatur provident quasi, vero!
+                        </div>
                     </div>
-                    <div class="j-card-item j-card-item-right">
-                        content right
-                    </div>
-                    <div class="j-card-item">
-                        content right
-                    </div>
-                </div>
+                    <span class="j-card-divider"></span>
 
-                <span class="j-card-divider"></span>
+                    <div class="j-card-item-group">  <!-- Possible ext class:  j-no-margin, j-no-padding -->
+                        <div class="j-card-item">
+                            <span>Hello</span>
+                        </div>
+                        <div class="j-card-item">
+                            <i class="fab fa-facebook"></i>
+                        </div>
+                        <div class="j-card-item j-card-item-right">
+                            content right
+                        </div>
+                        <div class="j-card-item">
+                            content right
+                        </div>
+                    </div>
 
-                <div class="j-card-btn-group"> <!-- Possible ext class: j-no-padding -->
-                    <a class="btn btn-primary" href="#">Button 1 Button 1</a>
-                    <button class="btn"> <i class="icon fas fa-cog"></i> Button 2</button>
-                </div>
-                <div class="j-card-footer j-card-footer-lg">
-                    <div class="j-card-footer-item">
-                        <a href="#"> <i class="j-icon-lg fas fa-cloud-download-alt"></i> Details Information</a>
+                    <span class="j-card-divider"></span>
+
+                    <div class="j-card-btn-group"> <!-- Possible ext class: j-no-padding -->
+                        <a class="btn btn-primary" href="#">Button 1 Button 1</a>
+                        <button class="btn"> <i class="icon fas fa-cog"></i> Button 2</button>
+                    </div>
+                    <div class="j-card-footer j-card-footer-lg">
+                        <div class="j-card-footer-item">
+                            <a href="#"> <i class="j-icon-lg fas fa-cloud-download-alt"></i> Details Information</a>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <div class="col-4">
+                <div class="j-card j-card-has-hover">
+                    <div class="j-card-header j-card-header-sm">
+                        <div class="j-card-header-right">
+                            <button class="j-card-header-icon fas fa-ellipsis-h"></button>
+                        </div>
+                    </div>
+                    <div class="j-card-overview-box j-card-overview-warning">
+                        <div class="j-card-overview-icon">
+                            <i class="fab fa-invision"></i>
+                        </div>
+                        <div class="j-card-overview-content">
+                            65 <sub>Article</sub>
+                        </div>
+                    </div>
+                    <div class="j-card-footer j-card-footer-lg">
+                        <div class="j-card-footer-item">
+                            <a href="#"> <i class="j-icon-lg fas fa-cloud-download-alt"></i> Details Information</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
-    </div>
+
 
 
     </section>
