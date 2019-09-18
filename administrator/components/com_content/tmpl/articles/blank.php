0ac105af7421f12dc3996ca678f2fa20b9c0eacd
Delowar Hosain <delowar6494@gmail.com>
2019-09-18 20:18:43 +0600

    New iconic button added

--- administrator/components/com_content/tmpl/articles/blank.php
+++ administrator/components/com_content/tmpl/articles/blank.php
@@ -703,6 +703,7 @@ HTMLHelper::_('webcomponent', 'system/joomla-alert.min.js', array('version'=> 'a
                                 <h4>Obi-Wan Kenobi <span>(Admin)</span></h4>
                                 <h5 class="jcard-activity-type">Wrote Article: Service</h5>
                                 <p>You can select from a number of options for customising the look of your templates. The Template Manager supports…</p>
+                                <a href="#" class="iconic-button"><span class="fas fa-pen-alt"></span></a>
                             </div>
                         </div>
                     </div>
