fd1b8766a74dc55702eb0a5a939d30d3de6d987b
kawshar <kawshar@joomshaper.com>
2019-09-24 18:18:35 +0600

    content module

--- administrator/modules/mod_content/tmpl/default.php
+++ administrator/modules/mod_content/tmpl/default.php
@@ -20,18 +20,15 @@ HTMLHelper::_('webcomponent', 'system/joomla-tab.min.js', array('version'=> 'aut
 	<section orientation="vertical" id="module-content-latest-<?php echo $module->id; ?>" name="<?php echo Text::_('MOD_CONTENT_LATEST'); ?>">
 		<table class="table j-list-table" id="<?php echo str_replace(' ', '', $module->title) . $module->id; ?>">
 			<caption class="sr-only"><?php echo $module->title; ?></caption>
-			<thead>
-				<tr>
-					<th scope="col" style="width:60%"><?php echo Text::_('JGLOBAL_TITLE'); ?></th>
-					<th scope="col" style="width:20%"><?php echo Text::_('JAUTHOR'); ?></th>
-					<th scope="col" style="width:20%"><?php echo Text::_('JDATE'); ?></th>
-				</tr>
-			</thead>
 			<tbody>
 				<?php if (count($latests)) : ?>
 				<?php foreach ($latests as $i => $item) : ?>
 				<tr>
-					<th scope="row">
+					<th scope="row" width="100px">
+						<?php echo Text::_('JGRID_HEADING_ID'); ?>
+						<small class="text-munted">(<?php echo $item->id; ?>)</small>
+					</th>
+					<td>
 						<?php if ($item->checked_out) : ?>
 							<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time); ?>
 						<?php endif; ?>
@@ -42,11 +39,8 @@ HTMLHelper::_('webcomponent', 'system/joomla-tab.min.js', array('version'=> 'aut
 						<?php else : ?>
 							<?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>
 						<?php endif; ?>
-					</th>
-					<td>
-						<?php echo $item->author_name; ?>
 					</td>
-					<td>
+					<td width="20%" class="text-right">
 						<?php echo HTMLHelper::_('date', $item->publish_up, Text::_('DATE_FORMAT_LC4')); ?>
 					</td>
 				</tr>
@@ -54,7 +48,7 @@ HTMLHelper::_('webcomponent', 'system/joomla-tab.min.js', array('version'=> 'aut
 				<?php else : ?>
 				<tr>
 					<td colspan="3">
-						<?php echo Text::_('MOD_LATEST_NO_MATCHING_RESULTS'); ?>
+						<?php echo Text::_('MOD_CONTENT_NO_MATCHING_RESULTS'); ?>
 					</td>
 				</tr>
 				<?php endif; ?>
@@ -64,18 +58,15 @@ HTMLHelper::_('webcomponent', 'system/joomla-tab.min.js', array('version'=> 'aut
 	<section id="module-content-popular-<?php echo $module->id; ?>" name="<?php echo Text::_('MOD_CONTENT_POPULAR'); ?>">
 		<table class="table j-list-table" id="<?php echo str_replace(' ', '', $module->title) . $module->id; ?>">
 			<caption class="sr-only"><?php echo $module->title; ?></caption>
-			<thead>
-				<tr>
-					<th scope="col" style="width:60%"><?php echo Text::_('JGLOBAL_TITLE'); ?></th>
-					<th scope="col" style="width:20%"><?php echo Text::_('JAUTHOR'); ?></th>
-					<th scope="col" style="width:20%"><?php echo Text::_('JDATE'); ?></th>
-				</tr>
-			</thead>
 			<tbody>
 				<?php if (count($popular)) : ?>
 				<?php foreach ($popular as $i => $item) : ?>
 				<tr>
-					<th scope="row">
+					<th scope="row" width="100px">
+						<?php echo Text::_('JGRID_HEADING_ID'); ?>
+						<small class="text-munted">(<?php echo $item->id; ?>)</small>
+					</th>
+					<td scope="row">
 						<?php if ($item->checked_out) : ?>
 							<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time); ?>
 						<?php endif; ?>
@@ -86,19 +77,17 @@ HTMLHelper::_('webcomponent', 'system/joomla-tab.min.js', array('version'=> 'aut
 						<?php else : ?>
 							<?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>
 						<?php endif; ?>
-					</th>
-					<td>
-						<?php echo $item->author_name; ?>
 					</td>
-					<td>
-						<?php echo HTMLHelper::_('date', $item->publish_up, Text::_('DATE_FORMAT_LC4')); ?>
+					<td width="20%" class="text-right">
+						<?php echo Text::_('JGLOBAL_HITS'); ?>
+						<span class="badge badge-warning"><?php echo $item->id; ?></span>
 					</td>
 				</tr>
 				<?php endforeach; ?>
 				<?php else : ?>
 				<tr>
 					<td colspan="3">
-						<?php echo Text::_('MOD_LATEST_NO_MATCHING_RESULTS'); ?>
+						<?php echo Text::_('MOD_CONTENT_NO_MATCHING_RESULTS'); ?>
 					</td>
 				</tr>
 				<?php endif; ?>
