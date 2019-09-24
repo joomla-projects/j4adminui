5617239895bfcac3438b49a2172ea9541ee19f55
Sajeeb Ahamed <sajeeb07ahamed@gmail.com>
2019-09-24 12:22:16 +0600

    Fix searchtools issue and extension-updates modules language strings

--- administrator/components/com_menus/layouts/joomla/searchtools/default.php
+++ administrator/components/com_menus/layouts/joomla/searchtools/default.php
@@ -19,7 +19,7 @@ $data = $displayData;
 // client_id filed options
 $clientOptions = $data['view']->filterForm->getField('client_id')->options ?? array();
 $state = $data['view']->get('State');
-$defaultValue =  !empty($clientOptions) ? $state->get('client_id', 0) : 0;
+$defaultValue =  !empty($clientOptions) ? $state->get('filter.client_id', 0) : 0;
 
 // Receive overridable options
 $data['options'] = !empty($data['options']) ? $data['options'] : array();
@@ -101,7 +101,7 @@ HTMLHelper::_('searchtools.form', $data['options']['formSelector'], $data['optio
 		<?php if(!empty($clientOptions)) : ?>
 			<div class="btn-group btn-group-border mr-3" role="group">
 				<?php foreach($clientOptions as $key => $option) : ?>
-					<button type="button" class="js-stools-selector-btn btn <?php echo $defaultValue == $option->value ? 'btn-default' : 'btn-link'; ?>" value="<?php echo $option->value; ?>"><?php echo $option->text; ?></button>
+					<button type="button" class="js-stools-selector-btn btn <?php echo $defaultValue == $option->value ? 'btn-primary' : 'btn-secondary'; ?>" value="<?php echo $option->value; ?>"><?php echo $option->text; ?></button>
 				<?php endforeach; ?>
 			</div>
 			<input type="hidden" value="<?php echo $defaultValue; ?>" class="js-stools-selector-client-id-field" name="client_id" />
