86f01eff0420b478bee5a33f727466c6f28e44d6
Delowar Hosain <delowar6494@gmail.com>
2019-09-24 16:45:19 +0600

    module style updated

--- administrator/modules/mod_extension_updates/tmpl/default.php
+++ administrator/modules/mod_extension_updates/tmpl/default.php
@@ -22,71 +22,71 @@ $updateMsg = '';
 
 if (!empty($extensionContents['updatableInfo']['component']))
 {
-    $updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_COMPONENTS', $extensionContents['updatableInfo']['component']);
+	$updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_COMPONENTS', $extensionContents['updatableInfo']['component']);
 }
 
 if (!empty($extensionContents['updatableInfo']['module']))
 {
-    $updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_MODULES', $extensionContents['updatableInfo']['module']);
+	$updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_MODULES', $extensionContents['updatableInfo']['module']);
 }
 
 if (!empty($extensionContents['updatableInfo']['plugin']))
 {
-    $updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_PLUGINS', $extensionContents['updatableInfo']['plugin']);
+	$updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_PLUGINS', $extensionContents['updatableInfo']['plugin']);
 }
 
 if (!empty($extensionContents['updatableInfo']['library']))
 {
-    $updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_LIBRARIES', $extensionContents['updatableInfo']['library']);
+	$updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_LIBRARIES', $extensionContents['updatableInfo']['library']);
 }
 
 if (!empty($extensionContents['updatableInfo']['package']))
 {
-    $updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_PACKAGES', $extensionContents['updatableInfo']['package']);
+	$updateMsg .= Text::plural('MOD_EXTENSION_UPDATES_PACKAGES', $extensionContents['updatableInfo']['package']);
 }
 
 ?>
 
 <div class="mod-extension-updates text-center module-<?php echo $module->id; ?>" id="mod-extension-updates-<?php echo $module->id; ?>">
-    <div class="mod-eu-joomla-update">
-        <?php if($extensionContents['updateJoomla']) : ?>
-            <h3 class="text-warning"><span class="fas fa-exclamation-triangle" area-hidden="true"></span> Your Joomla <?php echo JVERSION; ?> is Outdated</h3>
-            <a href="<?php echo Route::_('index.php?option=com_joomlaupdate'); ?>"><?php echo Text::_('Update to Joomla 4.2'); ?></a>
-        <?php else : ?>
-        <div class="updated">
-            <h3 class="mb-4"><span class="fas fa-check-circle text-success" area-hidden="true"></span> Your Joomla is up to date to the latest version <?php echo JVERSION; ?></h3>
-        </div>
-        <?php endif; ?> 
-    </div><!-- /.mod-eu-joomla-update -->
+	<div class="mod-eu-joomla-update">
+		<?php if($extensionContents['updateJoomla']) : ?>
+			<h3 class="text-warning"><span class="fas fa-exclamation-triangle" area-hidden="true"></span> Your Joomla <?php echo JVERSION; ?> is Outdated</h3>
+			<a href="<?php echo Route::_('index.php?option=com_joomlaupdate'); ?>"><?php echo Text::_('Update to Joomla 4.2'); ?></a>
+		<?php else : ?>
+			<div class="updated">
+				<h3 class="mb-4"><span class="fas fa-check-circle text-success" area-hidden="true"></span> Your Joomla is up to date to the latest version <?php echo JVERSION; ?></h3>
+			</div>
+		<?php endif; ?>
+	</div><!-- /.mod-eu-joomla-update -->
 
-    <div class="mod-eu-extension-update">
-        <!-- <div class="mod-eu-percent">
+	<div class="mod-eu-extension-update">
+		<!-- <div class="mod-eu-percent">
             <h4><?php echo $extensionContents['percentage'] . '%'; ?></h4>
             <p><?php echo Text::_('MOD_EXTENSION_UPDATES_UPDATED'); ?></p>
         </div> -->
 
-        <div class="j-progress">
-            <joomla-progress progress="<?php echo $extensionContents['percentage']; ?>" radius="80" stroke="15" duration="2000" >
-                <h2><span data-counter="true"></span>%</h2>
-                <span>Updated</span>
-            </joomla-progress>
-        </div>
+		<div class="j-progress">
+			<joomla-progress progress="<?php echo $extensionContents['percentage']; ?>" radius="80" stroke="15" duration="2000" >
+				<h2><span data-counter="true"></span>%</h2>
+				<span>Updated</span>
+			</joomla-progress>
+		</div>
 
-        <?php if(!empty($extensionContents['updatableInfo']['component']) || 
-            !empty($extensionContents['updatableInfo']['module']) ||
-            !empty($extensionContents['updatableInfo']['plugin']) ||
-            !empty($extensionContents['updatableInfo']['library']) ||
-            !empty($extensionContents['updatableInfo']['package'])) : ?>
-            <div class="mod-eu-group-count">
-                <p class=""><?php echo Text::sprintf('MOD_EXTENSION_UPDATES_UPDATE_EXTENSIONS_MSG', $updateMsg); ?></p>
-            </div>
-            <div class="mod-eu-group-update-all">
-                <a href="<?php echo Route::_('index.php?option=com_installer&view=update'); ?>" class="btn btn-link"><span class="fa fa-sync"></span> <?php echo JText::_('MOD_EXTENSION_UPDATES_UPDATE_ALL'); ?></a>
-            </div>
-        <?php else: ?>
-            <div class="mod-eu-uptodate">
-                <span class="fa fa-check-circle"></span> <?php echo Text::_('MOD_EXTENSION_UPDATES_UPTODATE'); ?>
-            </div>
-        <?php endif; ?>
-    </div><!-- /.mod-eu-extension-update -->
+		<?php if(!empty($extensionContents['updatableInfo']['component']) ||
+			!empty($extensionContents['updatableInfo']['module']) ||
+			!empty($extensionContents['updatableInfo']['plugin']) ||
+			!empty($extensionContents['updatableInfo']['library']) ||
+			!empty($extensionContents['updatableInfo']['package'])) : ?>
+			<div class="mod-eu-group-count">
+				<p class=""><?php echo Text::sprintf('MOD_EXTENSION_UPDATES_UPDATE_EXTENSIONS_MSG', $updateMsg); ?></p>
+			</div>
+			<div class="mod-eu-group-update-all">
+				<a href="<?php echo Route::_('index.php?option=com_installer&view=update'); ?>" class="btn btn-link"><span class="fa fa-sync"></span> <?php echo JText::_('MOD_EXTENSION_UPDATES_UPDATE_ALL'); ?></a>
+			</div>
+		<?php else: ?>
+			<div class="mod-eu-uptodate">
+				<span class="fa fa-check-circle"></span> <?php echo Text::_('MOD_EXTENSION_UPDATES_UPTODATE'); ?>
+			</div>
+		<?php endif; ?>
+	</div><!-- /.mod-eu-extension-update -->
 </div>
