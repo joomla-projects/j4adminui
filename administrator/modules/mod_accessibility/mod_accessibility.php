c271b2708062e664b67faa91c4e34dd650653c34
Rifat Wahid Alif <rifat3280@gmail.com>
2019-10-04 20:00:48 +0600

    mod_accessibility module created and working for accessibility

---
+++ administrator/modules/mod_accessibility/mod_accessibility.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_accessibility
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Helper\ModuleHelper;
+
+require ModuleHelper::getLayoutPath('mod_accessibility', $params->get('layout', 'default'));
---
+++ administrator/modules/mod_accessibility/tmpl/default.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_accessibility
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+?>
+
+<a class="header-item-link" href="" target="_blank">
+    <span class="header-item-icon fab fa-accessible-icon" aria-hidden="true"></span>
+    <span class="header-item-text">
+        <?php echo Text::_('MOD_ACCESSIBILITY'); ?>
+    </span>
+</a>
