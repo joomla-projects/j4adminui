84d9336e6905af03b7c0a78a039ecaa009d6dc22
Delowar Hosain <delowar6494@gmail.com>
2019-10-10 19:20:17 +0600

    module design updated

--- administrator/modules/mod_resources/tmpl/default.php
+++ administrator/modules/mod_resources/tmpl/default.php
@@ -69,47 +69,47 @@ $resourceStatus = $params->get('show_resources', 'both');
         <div class="resources-card-container mt-4">
             <div class="j-card">
                 <div class="j-card-body">
-                    <div class="row">
-                        <div class="col-12 col-md-6 col-lg-4 col-xl-3 mb-3 mb-md-0">
+                    <div class="row align-items-center justify-content-center justify-content-xl-start">
+                        <div class="col-12 col-xl-auto mb-5 mb-xl-0 text-center text-xl-left">
                             <?php echo HTMLHelper::_('image', 'mod_resources/resource-pay.png', 'Resources pay', array('class' => "mod-resources-image"), true); ?>
                         </div>
                         <div class="col">
                             <div class="row mt-n3">
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/creative-strategy" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_CREATIVE_STRATEGY'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/creative-strategy" target="_blank"><?php echo Text::_('MOD_RESOURCES_CREATIVE_STRATEGY'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/content-development" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_CONTENT_DEVELOPMENT'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/content-development" target="_blank"><?php echo Text::_('MOD_RESOURCES_CONTENT_DEVELOPMENT'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://developer.joomla.org/development-strategy.html" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_DEVELOPMENT'); ?></a>
+                                    <a href="https://developer.joomla.org/development-strategy.html" target="_blank"><?php echo Text::_('MOD_RESOURCES_DEVELOPMENT'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/configuration-support" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_CONFIGURATION'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/configuration-support" target="_blank"><?php echo Text::_('MOD_RESOURCES_CONFIGURATION'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/hosting-providers" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_HOSTING_PROVIDERS'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/hosting-providers" target="_blank"><?php echo Text::_('MOD_RESOURCES_HOSTING_PROVIDERS'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/design-development" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_DESIGN'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/design-development" target="_blank"><?php echo Text::_('MOD_RESOURCES_DESIGN'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://extensions.joomla.org/" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_EXTENSION'); ?></a>
+                                    <a href="https://extensions.joomla.org/"><?php echo Text::_('MOD_RESOURCES_EXTENSION'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/joomla-security" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_JOOMLA_SECURITY'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/joomla-security" target="_blank"><?php echo Text::_('MOD_RESOURCES_JOOMLA_SECURITY'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/technical-support" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_TECHNICAL_SUPPORT'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/technical-support" target="_blank"><?php echo Text::_('MOD_RESOURCES_TECHNICAL_SUPPORT'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/consulting" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_CONSULTING'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/consulting" target="_blank"><?php echo Text::_('MOD_RESOURCES_CONSULTING'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/migration-and-upgrade-services" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_MIGRATIONS'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/migration-and-upgrade-services" target="_blank"><?php echo Text::_('MOD_RESOURCES_MIGRATIONS'); ?></a>
                                 </div>
                                 <div class="col-12 col-md-auto mt-3">
-                                    <a href="https://resources.joomla.org/en/category/project-management" target="_blank" class="btn btn-default btn-block"><?php echo Text::_('MOD_RESOURCES_PROJECT_MANAGEMENT'); ?></a>
+                                    <a href="https://resources.joomla.org/en/category/project-management" target="_blank"><?php echo Text::_('MOD_RESOURCES_PROJECT_MANAGEMENT'); ?></a>
                                 </div>
                             </div>
                             <!-- <div class="row mt-3">
