60db7c693638ff41421a4dfdd77540d63ff93d2c
Rifat Wahid Alif <rifat3280@gmail.com>
2019-10-11 21:33:14 +0600

    fix disable cache javascript error issue.

--- build/media_source/mod_cache/js/cache.es6.js
+++ build/media_source/mod_cache/js/cache.es6.js
@@ -17,43 +17,46 @@
   };
   const theClearBtn = document.querySelector(selectors.clearBtn);
   const theCacheAnimationBtn = document.querySelector(selectors.cacheAnimationBtn);
-  theClearBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    // $(this).data('id')
-    const cacheurl = Joomla.getOptions('cacheurl');
-    const cashsize = theClearBtn.getAttribute('data-size');
-    const params = '';
-    Joomla.request({
-      url: cacheurl,
-      method: 'POST',
-      params,
-      perform: true,
-      onSuccess(res) {
-        const response = typeof res === 'string' && res.length > 0 ? JSON.parse(res) : false;
-        const { data } = response;
-        if (data.status) {
-          // add disabled attribs and class after clear the cache
-          theClearBtn.setAttribute('disabled', '');
-          theClearBtn.className += ' disabled';
-          // get cache current size
-          let currentCashSize = Math.round(cashsize);
-          const clearedCashSize = 0;
-          // cache size animation
-          const interval = setInterval(() => {
-            theCacheAnimationBtn.textContent = currentCashSize;
-            if (currentCashSize <= clearedCashSize) {
-              clearInterval(interval);
-              // show success messages which getting from helper
-              Joomla.renderMessages({ message: [data.messsage] });
-            }
-            // eslint-disable-next-line no-plusplus
-            --currentCashSize;
-          }, 1);
-        }
-      },
-      onError(xhr) {
-        Joomla.renderMessages({ error: [xhr.response] });
-      },
+  
+  if (theClearBtn !== null) {
+    theClearBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      // $(this).data('id')
+      const cacheurl = Joomla.getOptions('cacheurl');
+      const cashsize = theClearBtn.getAttribute('data-size');
+      const params = '';
+      Joomla.request({
+        url: cacheurl,
+        method: 'POST',
+        params,
+        perform: true,
+        onSuccess(res) {
+          const response = typeof res === 'string' && res.length > 0 ? JSON.parse(res) : false;
+          const { data } = response;
+          if (data.status) {
+            // add disabled attribs and class after clear the cache
+            theClearBtn.setAttribute('disabled', '');
+            theClearBtn.className += ' disabled';
+            // get cache current size
+            let currentCashSize = Math.round(cashsize);
+            const clearedCashSize = 0;
+            // cache size animation
+            const interval = setInterval(() => {
+              theCacheAnimationBtn.textContent = currentCashSize;
+              if (currentCashSize <= clearedCashSize) {
+                clearInterval(interval);
+                // show success messages which getting from helper
+                Joomla.renderMessages({ message: [data.messsage] });
+              }
+              // eslint-disable-next-line no-plusplus
+              --currentCashSize;
+            }, 1);
+          }
+        },
+        onError(xhr) {
+          Joomla.renderMessages({ error: [xhr.response] });
+        },
+      });
     });
-  });
+  }
 })(window.Joomla, document);
