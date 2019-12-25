7b65e2b85c1bfe00d9052c467b4a8246def2f09c
giangndg <giangnd@bravebits.vn>
2019-12-25 18:12:47 +0700

    Fix issue #121: Logged in users - revert all changes at core file and create an mod_logged override

--- administrator/templates/khonsu/html/mod_logged/default.php
+++ administrator/templates/khonsu/html/mod_logged/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  mod_logged
+ * @subpackage  Templates.khonsu
  *
  * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -23,7 +23,7 @@ use Joomla\CMS\Language\Text;
 						</div>
 					</div>
 
-					<div class="col-6 col-lg-3">
+					<div class="col-6 col-lg-4">
 						<?php if (isset($user->editLink)) : ?>
 							<a href="<?php echo $user->editLink; ?>">
 								<strong><?php echo htmlspecialchars($user->name, ENT_QUOTES, 'UTF-8'); ?></strong>
@@ -37,7 +37,7 @@ use Joomla\CMS\Language\Text;
 						</div>
 					</div>
 
-					<div class="d-none d-lg-block col-lg-3">
+					<div class="d-none d-lg-block col-lg-4">
 						<?php if ($user->client_id === null) : ?>
 							<?php // Don't display a client ?>
 						<?php elseif ($user->client_id) : ?>
@@ -52,11 +52,13 @@ use Joomla\CMS\Language\Text;
 					</div>
 
 					<div class="col-auto ml-auto">
-						<form action="<?php echo $user->logoutLink; ?>" method="post" name="adminForm">
-							<button type="submit" class="btn btn-link btn-sm m-n2">
-								<span class="j-card-icon icon-exit"></span> <?php echo Text::_('JLOGOUT'); ?>
-							</button>
-						</form>
+						<?php if (!$user->client_id) : ?>	
+							<form action="<?php echo $user->logoutLink; ?>" method="post" name="adminForm">
+								<button type="submit" class="btn btn-link btn-sm m-n2">
+									<span class="j-card-icon icon-exit"></span> <?php echo Text::_('JLOGOUT'); ?>
+								</button>
+							</form>
+						<?php endif; ?>
 					</div>
 
 				</div>
